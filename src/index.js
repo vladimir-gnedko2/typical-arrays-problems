92d36d3b715ade369c53be58e871e0c7b0d869ab
vladimir-gnedko2 <vavanchikmks@gmail.com>
2020-10-01 00:17:30 +0300

    add: file index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (!array || !array.length) return 0;
+  return Math.min.apply(Math, array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!array || !array.length) return 0;
+  return Math.max.apply(Math, array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (!array || !array.length) return 0;
+  return array.reduce((a, b) => a + b, 0) / array.length;
+};
